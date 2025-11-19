9635e28e16223ea13145b9e9cfe47f6cbd9fec6a
Alina Shevchenko <alisashef@gmail.com>
2025-11-19 20:30:46 +0200

    Updates

---
+++ assets/swiper-production-Cs39DeBJ.js
@@ -0,0 +1,2 @@
+import{S as o,N as r}from"./vendor-CPOeuTnm.js";function e(e){new o(`${e} .swiper`,{modules:[r],loop:!0,navigation:{nextEl:`${e} .swiper-production__btn_next`,prevEl:`${e} .swiper-production__btn_prev`},grabCursor:!0,speed:700})}export{e as initSwiper};
+//# sourceMappingURL=swiper-production-Cs39DeBJ.js.map
