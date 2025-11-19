0e3c255866a2fa8332226a8bd7b66d09acc320d4
Alina Shevchenko <alisashef@gmail.com>
2025-11-19 18:45:46 +0200

    Updates

---
+++ assets/swiper-lazy-loading-C6_8K6q9.js
@@ -0,0 +1,2 @@
+import{b as e}from"./index-CSt9eDSD.js";function n(n){n.querySelectorAll(".img[data-src]").forEach(n=>{e(n)})}function o(e,o=2){const s=e.slides.length,t=()=>{const t=e.activeIndex;for(let c=-o;c<=o;c++){const o=(t+c+s)%s,i=e.slides[o];i&&n(i)}};t(),e.on("slideChange",t)}export{o as l};
+//# sourceMappingURL=swiper-lazy-loading-C6_8K6q9.js.map
---
+++ assets/swiper-production-CBkqG9PR.js
@@ -0,0 +1,2 @@
+import{S as o,N as r}from"./vendor-ULRMf_3L.js";import{l as e}from"./swiper-lazy-loading-C6_8K6q9.js";import"./index-CSt9eDSD.js";function n(n){const i=new o(`${n} .swiper`,{modules:[r],loop:!0,navigation:{nextEl:`${n} .swiper-production__btn_next`,prevEl:`${n} .swiper-production__btn_prev`},grabCursor:!0,speed:700});e(i)}export{n as initSwiper};
+//# sourceMappingURL=swiper-production-CBkqG9PR.js.map
