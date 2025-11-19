b95f228679212c1c1bc1ae89d346de25aabf7c3b
Alina Shevchenko <alisashef@gmail.com>
2025-11-19 20:14:17 +0200

    Updates

---
+++ assets/swiper-production-BgmfsKgX.js
@@ -0,0 +1,2 @@
+import{S as o,N as r}from"./vendor-CPOeuTnm.js";import{l as e}from"./swiper-lazy-loading-B7Q2zZxX.js";import"./index-CPdqMQjv.js";function n(n){const i=new o(`${n} .swiper`,{modules:[r],loop:!0,navigation:{nextEl:`${n} .swiper-production__btn_next`,prevEl:`${n} .swiper-production__btn_prev`},grabCursor:!0,speed:700});e(i)}export{n as initSwiper};
+//# sourceMappingURL=swiper-production-BgmfsKgX.js.map
