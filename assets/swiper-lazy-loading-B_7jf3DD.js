c3ecc9ac69559bcf665cf5add10cb226c163fec7
Alina Shevchenko <alisashef@gmail.com>
2025-11-19 18:34:57 +0200

    Updates

--- assets/swiper-lazy-loading-B_7jf3DD.js
+++
@@ -1,2 +0,0 @@
-import{b as e}from"./index-DV3dtqPh.js";function n(n){n.querySelectorAll(".img[data-src]").forEach(n=>{e(n)})}function o(e,o=2){const s=e.slides.length,t=()=>{const t=e.activeIndex;for(let c=-o;c<=o;c++){const o=(t+c+s)%s,i=e.slides[o];i&&n(i)}};t(),e.on("slideChange",t)}export{o as l};
-//# sourceMappingURL=swiper-lazy-loading-B_7jf3DD.js.map
