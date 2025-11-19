39cc1a0fe8a10601851b4aca9b1bdad1643a3146
Alina Shevchenko <alisashef@gmail.com>
2025-11-19 19:25:05 +0200

    Updates

--- assets/swiper-lazy-loading-BOJBwDV6.js
+++
@@ -1,2 +0,0 @@
-import{b as e}from"./index-CucTlBT_.js";function n(n){n.querySelectorAll(".img[data-src]").forEach(n=>{e(n)})}function o(e,o=2){const s=e.slides.length,t=()=>{const t=e.activeIndex;for(let c=-o;c<=o;c++){const o=(t+c+s)%s,i=e.slides[o];i&&n(i)}};t(),e.on("slideChange",t)}export{o as l};
-//# sourceMappingURL=swiper-lazy-loading-BOJBwDV6.js.map
