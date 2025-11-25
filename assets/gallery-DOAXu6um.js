7ae0790571f42b68abea300732687acd46817ad9
Alina Shevchenko <alisashef@gmail.com>
2025-11-25 19:42:39 +0200

    Updates

--- assets/gallery-DOAXu6um.js
+++ assets/gallery-DOAXu6um.js
@@ -1,2 +1,2 @@
 import{r as o}from"./reuse-functions-DplpQI1Z.js";import{f as r}from"./flip-card-7hba7igv.js";import"./vendor-CPOeuTnm.js";document.addEventListener("DOMContentLoaded",()=>{o(),r(".gallery-photo__card")});
-//# sourceMappingURL=gallery-Cw_dZAi4.js.map
+//# sourceMappingURL=gallery-DOAXu6um.js.map
