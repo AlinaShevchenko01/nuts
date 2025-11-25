16eb7f78c7ee9eb6b5276ee19288af5c7eec57dc
Alina Shevchenko <alisashef@gmail.com>
2025-11-25 22:49:53 +0200

    Updates

--- assets/for-customers-CWZytOa_.js
+++ assets/for-customers-CWZytOa_.js
@@ -1,2 +1,2 @@
 import{r as e}from"./reuse-functions-DplpQI1Z.js";import"./vendor-CPOeuTnm.js";document.addEventListener("DOMContentLoaded",()=>{e()});
-//# sourceMappingURL=for-customers-BbZJLiFJ.js.map
+//# sourceMappingURL=for-customers-CWZytOa_.js.map
