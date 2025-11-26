6b6aeb693479e084b086a4e27202eac1ec807342
Alina Shevchenko <alisashef@gmail.com>
2025-11-26 20:12:23 +0200

    Updates

--- assets/for-customers-BS7dcpiR.js
+++ assets/for-customers-BS7dcpiR.js
@@ -1,3 +1,3 @@
 const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/connect-swipers-Bg2rIOMu.js","assets/vendor-MLoxMhBq.js","assets/vendor-BTDLRqRw.css"])))=>i.map(i=>d[i]);
 import{r as o,_ as r}from"./reuse-functions-CalCxB26.js";import{u as s}from"./vendor-MLoxMhBq.js";document.addEventListener("DOMContentLoaded",()=>{o(),s(()=>r(()=>import("./connect-swipers-Bg2rIOMu.js"),__vite__mapDeps([0,1,2])),".for-customers-info__tabs",[],{threshold:0,rootMargin:"140px"})});
-//# sourceMappingURL=for-customers-D7mBZBX1.js.map
+//# sourceMappingURL=for-customers-BS7dcpiR.js.map
