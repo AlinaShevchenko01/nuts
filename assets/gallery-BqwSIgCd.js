8978aea4bc38518b9d9d94194a76f2a1abd500b8
Alina Shevchenko <alisashef@gmail.com>
2025-11-27 01:48:09 +0200

    Updates

---
+++ assets/gallery-BqwSIgCd.js
@@ -0,0 +1,2 @@
+import{r as o}from"./reuse-functions-4ZtkHQkF.js";import{f as r}from"./flip-card-7hba7igv.js";import"./vendor-MLoxMhBq.js";document.addEventListener("DOMContentLoaded",()=>{o(),r(".gallery-photo__card")});
+//# sourceMappingURL=gallery-BqwSIgCd.js.map
