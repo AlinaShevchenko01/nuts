0556d86d1f4a94e820efd6a6725faf624e593b8e
Alina Shevchenko <alisashef@gmail.com>
2025-12-03 01:45:42 +0200

    Updates

--- assets/pages/shop-BGZ2irpf.js
+++ assets/pages/shop-BGZ2irpf.js
@@ -1,2 +1,2 @@
 import{r as o,_ as r}from"../reuse-functions-Dk9e896E.js";import{u as s}from"../vendor-MLoxMhBq.js";document.addEventListener("DOMContentLoaded",async()=>{o(),s(()=>r(()=>import("../render-shop-card-BePJdp-2.js"),[]),".shop-hero",[],{threshold:0,rootMargin:"200px"}),s(()=>r(()=>import("../swiper-production-Bjrq_M5X.js"),[]),".shop-info",[".shop-info__swiper"],{threshold:0,rootMargin:"200px"})});
-//# sourceMappingURL=shop-Eg3vguhr.js.map
+//# sourceMappingURL=shop-BGZ2irpf.js.map
