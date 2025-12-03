bcee81fc8c87603383742e2cf144dbb25e44334b
Alina Shevchenko <alisashef@gmail.com>
2025-12-03 19:37:22 +0200

    Updates

--- assets/404-BLUSbwpu.js
+++ assets/404-BLUSbwpu.js
@@ -1,2 +1,2 @@
 import{r as e}from"./reuse-functions-CZ1yo8_i.js";import"./vendor-CdPkAZIS.js";document.addEventListener("DOMContentLoaded",async()=>{e()});
-//# sourceMappingURL=404-CEEuu_iY.js.map
+//# sourceMappingURL=404-BLUSbwpu.js.map
